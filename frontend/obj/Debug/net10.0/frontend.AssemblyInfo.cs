42dfb3c30b3b8e50da10599b2513d5e39e034cc8
André Escobar <andreescobara@gmail.com>
2026-04-30 14:27:25 -0600

    Pestaña de ayuda

--- frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net10.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d113cd6cf9ec42d2892875f77fb4bf90861059bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24162b81757397f88629e6697a4cca2741a11e3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
